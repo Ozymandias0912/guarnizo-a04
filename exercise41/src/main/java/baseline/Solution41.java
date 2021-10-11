f2069148a36fe1b354a1f146ff42349c45b30e8b
Juan <jdavid29a@gmail.com>
2021-10-11 16:11:39 -0400

    exercise 41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+package baseline;
+
+public class Solution41 {
+    /*
+    * Name Sorter
+    *
+    * //nameFile class
+    * name
+    * number of names
+    *
+     */
+    /*
+
+    * //nameFile() method
+    * //takes a file and reads the first line
+    * //saves the string into name
+    *
+    * //getName() method
+    * //prints whatever string is saved in name
+    *
+    * //getAllNames() method
+    * //while there is a line to be read from the file,
+    * //call nameFile and count the number of names
+      //print (Total of number names%n-----------------)
+
+      //while there is a line to be read from the file
+    * //call nameFile
+    * //call getName
+    *
+     /*
+    * //end nameFile class
+    *
+    * main class
+    * create nameFile object
+    *
+    * call get all names
+    *
+    *
+    *
+    * */
+}
