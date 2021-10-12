b205ac8b36c5fcd3ab67eb30c41cd677a10edbbc
Juan <jdavid29a@gmail.com>
2021-10-12 17:43:24 -0400

    pseudocode exercise 42

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+
+public class Solution42 {
+
+    /*
+    * Pseudocode
+    *   create a reader class
+    *   create a string list first names
+    *   create a string list last names
+    *   create an int list salaries
+    *   create a scanner input variable
+    *
+    *   read file method:
+    *   loop:
+    *   read from the file until a ,
+    *   save string in first name list
+    *   read from the file until a ,
+    *   save string in last name list
+    *   read from the file until newline
+    *   save in the int list
+    *
+    *   print method:
+    *   print headline of table
+    *   loop:
+    *   print first name
+    *   print last name
+    *   print salary
+    *
+    *
+    *
+    *
+    *
+    *
+    * */
+}
