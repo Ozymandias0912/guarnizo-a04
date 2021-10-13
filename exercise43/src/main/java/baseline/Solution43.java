5a40ca389025d8ddaa4a8e76bc092ae3834e24d9
Juan <jdavid29a@gmail.com>
2021-10-13 17:52:49 -0400

    added pseudocode exercise 43

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,43 @@
+package baseline;
+
+public class Solution43 {
+
+    /*
+    * Pseudocode
+    *
+    * fileManager class
+    * create Scanner variable
+    * create string name
+    * create string author
+    * create string css
+    * create string js
+    *
+    * constructor
+    *
+    * ask()
+    * ask for site name and author and save them
+    * ask if the user wants to create folders for css and js
+    * and save answers
+    *
+    *
+    *
+    * print()
+    * create and print folder for the website
+    * create and print html file
+    * create and print js folder if requested
+    * create and print css folder if requested
+    *
+    * Solution43 class
+    *
+    * main()
+    * create fileManager object
+    * call ask()
+    * call print()
+    *
+    *
+    *
+    *
+    *
+    *
+    * */
+}
