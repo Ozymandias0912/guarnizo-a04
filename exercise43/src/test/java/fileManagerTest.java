30f69f86234c6e06f93499928ffb03364017c0dc
Juan <jdavid29a@gmail.com>
2021-10-14 19:55:20 -0400

    added test code

--- exercise43/src/test/java/fileManagerTest.java
+++ exercise43/src/test/java/fileManagerTest.java
@@ -2,20 +2,35 @@ import baseline.fileManager;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.File;
+import java.io.IOException;
 
 public class fileManagerTest {
 
-    @Test
+    fileManager test = new fileManager();
+
+    @Test//testing constructor
     void constructorTest(){
-        fileManager test = new fileManager();
+
         test.fileManager();
 
-        //test constructor
+
         assertEquals("cs23", test.getWebsiteName());
         assertEquals("Juan Guarnizo", test.getAuthor());
         assertEquals("maybe", test.getCssAnswer());
         assertEquals("maybe", test.getJsAnswer());
 
+
+    }
+
+    void createFoldersTest(){
+
+        try {
+            test.createFolders();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //assertEquals(); how to do it?
 
     }
 
