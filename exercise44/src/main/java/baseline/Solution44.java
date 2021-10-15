9b6fd6381db3f92a5d7a6124340b9394e4387b9e
Juan <jdavid29a@gmail.com>
2021-10-14 20:29:30 -0400

    added pseudoce for exercise 44

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+package baseline;
+
+public class Solution44 {
+
+    /*
+    * PSEUDOCODE
+    *
+    *   //product class
+    *   String name
+    *   double price
+    *   int quantity
+    *
+    *
+    *   class() constructor
+    *
+    *   get methods
+    *   //end class
+    *
+    *   //GsonManager class
+    *   Gson gson
+    *   File f
+    *   String jsonArray
+    *   product[] productArray
+    *
+    *   GsonManager() constructor
+    *
+    *   productSearch()
+    *
+    *
+    * */
+}
