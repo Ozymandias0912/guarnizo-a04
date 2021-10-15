84ff29ee8abdfb5caa7459735444504d3e5a4d42
Juan <jdavid29a@gmail.com>
2021-10-15 13:23:14 -0400

    added pseudocode exercise 45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+package baseline;
+
+public class Solution45 {
+    /*
+    * Pseudocode
+    *
+    * //class file
+    * scanner input
+    *
+    *
+    * askInput()
+    * asks for the name of the input file
+    * set up the scanner input to read the file
+    *
+    * String replace(scanner input)
+    * reads the file
+    * copies the info and replaces utilize for use
+    * returns the newString with the replacements
+    *
+    * output(String newString)
+    * creates a blank file
+    * writes the newString into the file
+    *
+    *
+    *
+    *
+    *
+    * */
+}
